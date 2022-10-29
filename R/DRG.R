7849b10bc274972a89e670a335cca84be504b684
Wanni Lei <wal4002@med.cornell.edu>
2022-10-29 15:38:12 -0400

    add data to the package

--- R/DRG.R
+++ R/DRG.R
@@ -1,13 +1,16 @@
+library(ggplot2)
+library(tidyverse)
 
 ## function 1
 boxplot <- function(y){
-  DRG %>%
+
+  sample_data %>%
     select(drg_code, y=y) %>%
-    ggplot( mapping = aes(x= drg_code, y = y) )+ #  Make three payment as an option for function
+    ggplot(mapping = aes(x= drg_code, y = y) )+ #  Make three payment as an option for function
     geom_boxplot()+
     scale_x_discrete(guide = guide_axis(check.overlap = TRUE))+
     labs(x = "DRG code",
-         y = y,
+         y = "Payments",
          title = "Boxplot of payments by DRG code")  ## rename x, y axis and title
 
 }
@@ -20,20 +23,20 @@ boxplot <- function(y){
 statistics<- function(x){
 
   if(x=="mean"){
-    table <-drg_data %>%
+    table <- sample_data %>%
       summarise(mean = mean(Average.Medicare.Payments))
     table
   }
-  else if (x=="median"){
-    table <-drg_data %>%
+  else if (x == "median"){
+    table <- sample_data %>%
       summarise(median = median(Average.Medicare.Payments))
     table
   } else{
-    table <-drg_data %>%
+    table <- sample_data %>%
       summarise(SD = sd(Average.Medicare.Payments))
 
     table
   }
 }
 
-statistics("sd")
+## statistics("sd")
--- R/data.R
+++
@@ -1,6 +0,0 @@
-drg_data <- read.csv("DRG_data.csv")
-
-drg_data <- drg_data %>%
-  mutate(drg_code=substr(DRG.Definition,start=1,stop=3))
-
-save(drg_data, file = 'data/DRG.RData')
